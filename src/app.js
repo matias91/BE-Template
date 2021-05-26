65cb8706ed8f235a5c874ac865e46e9a6c9a010f
Matias <matias.marcelo.calvo@gmail.com>
2021-05-26 16:45:28 -0300

    Task 1

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const Sequelize = require('sequelize')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ * Returns the contract by id of the profile calling
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const profileId = req.get('profile_id')
+    const contract = await Contract.findOne({
+        where: { id, [Sequelize.Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }] }
+    });
+
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
 module.exports = app;
